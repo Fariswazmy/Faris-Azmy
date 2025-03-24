5c340f39e86e4672a82504c14f35d8e7bb163a21
Fariswazmy <fariswazmy@gmail.com>
2025-03-24 19:15:48 +0000

    done

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,14 @@
-print("hello")
+import random
+
+def request_user_name():
+    user_name = input ("Please enter your name here: ")
+    return user_name
+
+
+def random_digit():
+    upper_range = int(input("Set the maximum value for the range: "))
+    lower_range = int(input("Set the minimum value for the range: "))
+    return random.randint(lower_range, upper_range)
+
+
+print(f"{request_user_name()}, your random number is: {random_digit()}")
